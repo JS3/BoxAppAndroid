21683152dffeaccc3cf9e727414ca14e1d66a134
proyectoarsw <jsalas2345@hotmail.com>
2016-12-12 18:05:11 -0500

    clase viaje

--- BoxAppAndroid/app/src/main/java/com/example/ba/boxappandroid/ViajeAndroid.java
+++ BoxAppAndroid/app/src/main/java/com/example/ba/boxappandroid/ViajeAndroid.java
@@ -2,6 +2,11 @@ package com.example.ba.boxappandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by 2096904 on 12/12/16.
@@ -13,6 +18,31 @@ public class ViajeAndroid extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.viaje); {
-}
-    }
+            Button btnSelect = (Button) findViewById(R.id.btningresarv);
+            btnSelect.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    
+
+                    JSONObject json = new JSONObject();
+                    try {
+                        json.put("name", "emil");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        json.put("username", "emil111");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        json.put("age", "111");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+    });
+    }}
 }
