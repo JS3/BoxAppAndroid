6f54013d383cd82829f9dee2e989339b95cb034c
MstrJL300 <joslum3@icloud.com>
2016-12-13 13:04:00 -0500

    Recolección v3: Contiene una interface de botones para solicitar un pedido y entregarlo

--- app/src/main/java/recoleccion/RecoleccionActivity.java
+++ app/src/main/java/recoleccion/RecoleccionActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
+import com.example.ba.boxappandroid.Mensajero;
 import com.example.ba.boxappandroid.Pedido;
 import com.example.ba.boxappandroid.R;
+import com.example.ba.boxappandroid.Viaje;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,10 +29,12 @@ public class RecoleccionActivity extends AppCompatActivity {
     List<Pedido> listaPedidos = new ArrayList<>();
     List<Pedido> listaPedidosEspera;
     List<Pedido> listaPedidosEnviando;
+    List<Pedido> listaPedidosEntregado;
 
     List<Pedido> lista = new ArrayList<>();
 
     Button button;
+    Button peticion;
     RadioButton radioButton;
 
     TextView nombreProducto;
@@ -37,42 +42,62 @@ public class RecoleccionActivity extends AppCompatActivity {
     TextView origen;
     TextView destino;
 
+    LinearLayout linearLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recoleccion_view);
 
         button = (Button) findViewById(R.id.button);
+        peticion = (Button) findViewById(R.id.peticion);
         radioButton = (RadioButton) findViewById(R.id.radioButton);
         nombreProducto = (TextView) findViewById(R.id.nombreProducto);
         peso = (TextView) findViewById(R.id.peso);
         origen = (TextView) findViewById(R.id.origen);
         destino = (TextView) findViewById(R.id.destino);
 
-        listaPedidos.add(new Pedido(
-                new Solicitante(3048, "José"),
-                new Producto("ORIGEN", "La Torre Oscura I", "Un libro escrito por Stephen King.",
-                        "Estados Unidos", "New York", "Colombia", "Bogota", 1.5, false, false))
-        );
+        linearLayout = (LinearLayout) findViewById(R.id.LinearLayout1);
 
-        configurarlistaProductosEspera();
-        lista = listaPedidosEspera;
-        configurarInformacion();
+        peticion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listaPedidos.add(
+                        new Pedido(
+                            new Solicitante(3048, "José"),
+                            new Producto("ORIGEN", "La Torre Oscura I", "Un libro escrito por Stephen King.",
+                                    "Estados Unidos", "New York", "Colombia", "Bogota", 1.5, false, false))
+                );
+                configurarlistaPedidosEspera();
+                lista = listaPedidosEspera;
+                configurarInformacion();
+
+                peticion.setVisibility(v.INVISIBLE);
+                linearLayout.setVisibility(v.VISIBLE);
+                button.setVisibility(v.INVISIBLE);
+                radioButton.setEnabled(true);
+                button.setEnabled(true);
+
+                button.setText("Entregar");
+                button.setBackgroundColor(Color.parseColor("#D3D3D3"));
+            }
+        });
 
         radioButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(radioButton.isChecked()) {
                     listaPedidos.get(0).setEstado("Enviando");
-                    configurarlistaProductosEspera();
-                    configurarlistaProductosEnviando();
+                    configurarlistaPedidosEspera();
+                    configurarlistaPedidosEnviando();
                     lista = listaPedidosEnviando;
                     configurarInformacion();
+
                     button.setVisibility(v.VISIBLE);
                     radioButton.setEnabled(false);
-//                System.out.println(button.getVisibility());
-//                ViewGroup layout = (ViewGroup) findViewById(R.id.LinearLayout1);
-//                Button button = new Button();
+
+                    listaPedidos.get(0).setMensajero(new Mensajero()); //Se agrega el mensajero
+                    listaPedidos.get(0).setViaje(new Viaje()); //Se agrega el viaje
                 }
             }
         });
@@ -83,6 +108,14 @@ public class RecoleccionActivity extends AppCompatActivity {
             public void onClick(View v) {
                 button.setBackgroundColor(Color.parseColor("#00FF00"));
                 button.setText("Entregado");
+                configurarlistaPedidosEnviando();
+                configurarlistaPedidosEntregado();
+                lista = listaPedidosEntregado;
+                listaPedidos.remove(0);
+                button.setEnabled(false);
+
+                peticion.setVisibility(v.VISIBLE);
+                linearLayout.setVisibility(v.INVISIBLE);
             }
         });
 
@@ -90,7 +123,7 @@ public class RecoleccionActivity extends AppCompatActivity {
         //Agregar boton de entregar.
     }
 
-    public void configurarlistaProductosEspera(){
+    public void configurarlistaPedidosEspera(){
         listaPedidosEspera = new ArrayList<>();
 
         for(Pedido p:listaPedidos){
@@ -99,12 +132,21 @@ public class RecoleccionActivity extends AppCompatActivity {
         }
     }
 
-    public void configurarlistaProductosEnviando(){
+    public void configurarlistaPedidosEnviando(){
         listaPedidosEnviando = new ArrayList<>();
 
         for(Pedido p:listaPedidos){
             if(p.getEstado() == "Enviando")
                 listaPedidosEnviando.add(p);
+        }
+    }
+
+    public void configurarlistaPedidosEntregado(){
+        listaPedidosEntregado = new ArrayList<>();
+
+        for(Pedido p:listaPedidos){
+            if(p.getEstado() == "Entregando")
+                listaPedidosEntregado.add(p);
         }
     }
 
