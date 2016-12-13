8c7976e17623414d35b3e5b81f33caf1b6111a4b
MstrJL300 <joslum3@icloud.com>
2016-12-13 10:16:45 -0500

    Recolección: +Datos locales

--- app/src/main/java/recoleccion/RecoleccionActivity.java
+++ app/src/main/java/recoleccion/RecoleccionActivity.java
@@ -2,6 +2,7 @@ package recoleccion;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.example.ba.boxappandroid.R;
@@ -11,12 +12,22 @@ import com.example.ba.boxappandroid.R;
  */
 
 public class RecoleccionActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recoleccion_view);
 
+        RadioButton radioButton = (RadioButton) findViewById(R.id.radioButton);
         TextView nombreProducto = (TextView) findViewById(R.id.nombreProducto);
+        TextView peso = (TextView) findViewById(R.id.peso);
+        TextView origen = (TextView) findViewById(R.id.origen);
+        TextView destino = (TextView) findViewById(R.id.destino);
+
         nombreProducto.setText("Torre Oscura I");
+        peso.setText("1.5"+"Kg");
+        origen.setText("Estados Unidos"+"/"+"New York");
+        destino.setText("Colombia"+"/"+"Bogota");
     }
 }
